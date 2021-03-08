32882358b36690948a53fefad99450ebc6c523ef
mustafay <mustafay@netas.com.tr>
2021-03-08 14:16:17 +0300

    Scanner stack changed to queue.

--- src/main/java/com/gjg/backend/Scanner.java
+++ src/main/java/com/gjg/backend/Scanner.java
@@ -6,24 +6,31 @@ import org.slf4j.LoggerFactory;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
-import java.util.Stack;
+import java.util.LinkedList;
+import java.util.Queue;
 
 @Component
 public class Scanner{
 
     private static final Logger log = LoggerFactory.getLogger(Scanner.class);
-    private static Stack<Task> taskQueue = new Stack<>();
+    private static Queue<Task> taskQueue = new LinkedList<>();
 
+    /**
+     *
+     * Scan the tasks every 1 second. And run the tasks in fifo order.
+     * These tasks are used for write data to mysql.
+     *
+     */
     @Scheduled(fixedRate = 1000)
     public void scanTasks() {
-        while (!taskQueue.empty()) {
-            Task task = taskQueue.pop();
+        while (!taskQueue.isEmpty()) {
+            Task task = taskQueue.poll();
             log.info("Task found. Task name: " + task.taskName);
             task.givenTask.accept(task.parameter);
         }
     }
 
     public static void addTask(Task task) {
-        taskQueue.push(task);
+        taskQueue.offer(task);
     }
 }
